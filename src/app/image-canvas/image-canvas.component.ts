5511962ed46e94c59c54a66a4cb4e0cf45c7a4ea
shrey265 <shrma.shray@gmail.com>
2025-05-05 00:58:04 +0530

    Added state caching, reset button and history tracking

--- src/app/image-canvas/image-canvas.component.ts
+++ src/app/image-canvas/image-canvas.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule, NgStyle } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, EventEmitter, HostListener, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, HostListener, inject, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { StorageService } from '../service/storage.service';
 
 @Component({
   selector: 'app-image-canvas',
@@ -8,18 +9,18 @@ import { AfterViewInit, Component, ElementRef, EventEmitter, HostListener, Input
   templateUrl: './image-canvas.component.html',
   styleUrl: './image-canvas.component.scss'
 })
-export class ImageCanvasComponent implements AfterViewInit, OnChanges{
+export class ImageCanvasComponent implements AfterViewInit,OnInit, OnChanges{
   @ViewChild('canvas') canvas!: ElementRef;
   @Output('coords') coords: EventEmitter<any> = new EventEmitter();
   @Input('state') state: string = 'IDLE';
+  @Input('shiftHistory') shiftHistory: any[] = [];
+  @Input('scanHistory') scanHistory: any[] = [];
 
   // coords updated after each keypress
-  Xcoord: number = 0;
-  Ycoord: number = 0;
+  mappings: any = {'x': 0, 'y': 0};
 
   // coordinates where box will appear
-  Xfinal: number = 0;
-  Yfinal: number = 0;
+  finalCoords: any = {'x': 0, 'y': 0};
 
   // slide width and field width
   slideWidth: number = 0;
@@ -29,14 +30,23 @@ export class ImageCanvasComponent implements AfterViewInit, OnChanges{
   unitSizeY:  number = 0;
 
   // units moved in x and y directions
-  unitX: number = 0;
-  unitY: number = 0;
+  units: any = {'x': 0, 'y':0};
+
+  private storageService: StorageService = inject(StorageService);
 
   ngOnChanges(changes: SimpleChanges): void {
     if(changes['state'] && changes['state'].currentValue!='MOVING') {
-      this.Xfinal = this.Xcoord;
-      this.Yfinal = this.Ycoord;
-    }
+      this.finalCoords = this.mappings;
+      if(this.mappings.x!=0 || this.mappings.y!=0){
+        this.storageService.setItem("finalCoords", this.finalCoords);
+        this.storageService.setItem("mappings", this.mappings);
+      }
+    } 
+  }
+
+  ngOnInit(): void {
+    if(this.storageService.getItem("finalCoords")) this.finalCoords = this.storageService.getItem("finalCoords");
+    if(this.storageService.getItem("mappings")) this.mappings = this.storageService.getItem("mappings");
   }
 
   ngAfterViewInit(): void {
@@ -49,24 +59,27 @@ export class ImageCanvasComponent implements AfterViewInit, OnChanges{
   @HostListener('window:keydown', ['$event'])
   onKeyDown(event: KeyboardEvent) {
     if (event.keyCode === 39) {
-      if((this.Xcoord+this.unitSizeX)>=100) {this.Xcoord = 100-this.unitSizeX;return;}
-      this.Xcoord = this.Xcoord + this.unitSizeX;
-      this.unitX+=1;
+      if((this.mappings.x+this.unitSizeX)>=100) {this.mappings.x = 100-this.unitSizeX;return;}
+      this.mappings.x = this.mappings.x + this.unitSizeX;
+      this.units.x+=1;
     } else if (event.keyCode === 40) {
-      if(this.Ycoord+this.unitSizeY>=100) {this.Ycoord = 100 - this.unitSizeY;return;}
-      this.Ycoord = this.Ycoord +this.unitSizeY;
-      this.unitY+=1;
+      if(this.mappings.y+this.unitSizeY>=100) {this.mappings.y = 100 - this.unitSizeY;return;}
+      this.mappings.y = this.mappings.y +this.unitSizeY;
+      this.units.y+=1;
     } else if (event.keyCode === 37) {
-      if(this.Xcoord<=0) {this.Xcoord = 0;return;}
-      this.Xcoord = this.Xcoord -1*this.unitSizeX;
-      this.unitX-=1;
+      if(this.mappings.x<=0) {this.mappings.x = 0;return;}
+      this.mappings.x = this.mappings.x -1*this.unitSizeX;
+      this.units.x-=1;
     } else if (event.keyCode === 38) {
-      if(this.Ycoord<=0){this.Ycoord=0;return;}
-      this.Ycoord = this.Ycoord -1*this.unitSizeY;
-      this.unitY-=1;
+      if(this.mappings.y<=0){this.mappings.y=0;return;}
+      this.mappings.y = this.mappings.y -1*this.unitSizeY;
+      this.units.y-=1;
+    } else {
+      return;
     }
 
-    this.coords.emit({'unitX': this.unitX,'unitY': this.unitY});
+
+    this.coords.emit({'unitX': this.units.x,'unitY': this.units.y, 'xCoord': this.mappings.x, 'yCoord': this.mappings.y});
   }
 
 }
